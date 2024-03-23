497b4dc0fe601d3fd09dff3101e6bde779889b44
Rodrigo de Souza <rsouza01@gmail.com>
2024-03-23 12:25:10 +0000

    Small things changed

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,5 @@ export FLASK_APP=./${APP_FOLDER}/app.py
 
 echo "FLASK_APP: ${FLASK_APP}"
 
-pipenv run flask --debug run -h 0.0.0.0
+# pipenv run flask --debug run -h 0.0.0.0
+flask --debug run -h 0.0.0.0
--- smoketest.sh
+++ smoketest.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # sleep 5
 
 # get services
